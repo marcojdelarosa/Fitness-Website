ce50a9745fb99df70ae5706d9bc0c63d958419a0
Logan Hunt <l.b.hunt5@gmail.com>
2025-03-21 12:44:24 -0700

    Header v1 and css formatting for root element

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,24 @@
+import dots from "./assets/SidebarDots.svg";
+
+function HeaderWords() {
+  return (
+    <div className="container header">
+      <div className="headerleft">
+        <img id="headerdots" src={dots} alt="dots" draggable="false"></img>
+        <h1>CurPage</h1>
+      </div>
+      <div className="headerright">
+        <h1 id="headerlogoname">LOGO</h1>
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="headercontainer">
+      <HeaderWords />
+    </div>
+  );
+}
+export default Header;
