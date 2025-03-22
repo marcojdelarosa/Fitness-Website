177690c76e1c7cb1d112fa5982c9f43d85596813
McKay Hartman <mckayhartman@gmail.com>
2025-03-22 16:48:15 -0700

    Updated login page style and added buttons resolved #58 and resolved #70

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -3,16 +3,20 @@ import "./login.css";
 function Login(){
 
 	return (
+		
 		<div class="login-container">
+			
 			<form>
-
+				<h1>Log In</h1>
 				<label htmlFor="username">Username</label><br></br>
-				<input type="text" id="username" name="username" required /><br></br>
+				<input class="input-form" type="text" id="username" name="username" required /><br></br>
 
 				<label htmlFor="password">Password</label><br></br>
-				<input type="password" id="password" name="password" required /><br></br>
+				<input class="input-form" type="password" id="password" name="password" required /><br></br>
 
-				<button type="submit">Login</button>
+				<button class="login-button" type="submit">Log In</button><br></br>
+				<button type="button">Create Account</button>
+				<button type="guest">Continue as Guest</button>
 			</form>
 		</div>
 	);
