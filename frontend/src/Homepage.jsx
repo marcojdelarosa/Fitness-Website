ef1ffed718646b4b84479989dc5c7ccd56020775
Lacy Hamilton <lacyhamilton11@hotmail.com>
2025-03-22 23:59:50 -0700

    Created basic homepage.jsx and homepage.css for the homepage layout and styling. Resolves issue #71.

---
+++ frontend/src/Homepage.jsx
@@ -0,0 +1,27 @@
+import "./homepage.css";
+import Header from "./Header.jsx";
+// import image from './assets/pexels-victorfreitas-841128.jpg';
+
+function Homepage(){
+
+    return(
+        <>
+            <Header />
+            <div className="homepageContainer">
+                <header>
+                    <p>Welcome to our website.</p>
+                </header>
+
+                <div className="body">
+                    <img src={image} alt="Fitness Image"/>
+                </div>
+
+                <footer>
+                    <p>&copy; Copyright 2025</p>
+                </footer>
+            </div>
+        </>
+    );
+}
+
+export default Homepage
